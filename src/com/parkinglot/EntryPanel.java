722775f18146248739a487b532e8a2ba3190629e
Viswanatha Velayutham S <112478839+VISWANATHAVELAYUTHAM@users.noreply.github.com>
2024-08-06 17:24:27 +0530

    Update EntryPanel.java

--- src/com/parkinglot/EntryPanel.java
+++ src/com/parkinglot/EntryPanel.java
@@ -1,1 +1,13 @@
+class EntryPanel {
+    private int panelId;
+    private ParkingLot parkingLot;
 
+    public EntryPanel(int panelId, ParkingLot parkingLot) {
+        this.panelId = panelId;
+        this.parkingLot = parkingLot;
+    }
+
+    public Ticket generateTicket(Vehicle vehicle) {
+        return parkingLot.issueTicket(vehicle);
+    }
+}
