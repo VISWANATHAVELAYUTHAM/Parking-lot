78bda412bed6ddb25c696b911eb85a0a9a925760
Viswanatha Velayutham S <112478839+VISWANATHAVELAYUTHAM@users.noreply.github.com>
2024-08-06 17:24:54 +0530

    Update ExitPanel.java

--- src/com/parkinglot/ExitPanel.java
+++ src/com/parkinglot/ExitPanel.java
@@ -1,1 +1,15 @@
+class ExitPanel {
+    private int panelId;
+    private ParkingLot parkingLot;
 
+    public ExitPanel(int panelId, ParkingLot parkingLot) {
+        this.panelId = panelId;
+        this.parkingLot = parkingLot;
+    }
+
+    public double processPayment(Ticket ticket) {
+        double charge = parkingLot.calculateParkingCharge(ticket);
+        parkingLot.vacateSlot(ticket);
+        return charge;
+    }
+}
