98b371ec9ee79665b07bb30549e5f80121af0f23
Viswanatha Velayutham S <112478839+VISWANATHAVELAYUTHAM@users.noreply.github.com>
2024-08-06 17:23:50 +0530

    Update Admin.java

--- src/com/parkinglot/Admin.java
+++ src/com/parkinglot/Admin.java
@@ -1,1 +1,29 @@
 
+class Admin {
+    private ParkingLot parkingLot;
+
+    public Admin(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public void addFloor(int floorNumber) {
+        parkingLot.addFloor(new ParkingFloor(floorNumber));
+    }
+
+    public void addParkingSlot(int floorNumber, ParkingSlot slot) {
+        for (ParkingFloor floor : parkingLot.getFloors()) {
+            if (floor.getFloorNumber() == floorNumber) {
+                floor.addParkingSlot(slot);
+                return;
+            }
+        }
+    }
+
+    public void addEntryPanel(int panelId) {
+        parkingLot.addEntryPanel(new EntryPanel(panelId, parkingLot));
+    }
+
+    public void addExitPanel(int panelId) {
+        parkingLot.addExitPanel(new ExitPanel(panelId, parkingLot));
+    }
+}
