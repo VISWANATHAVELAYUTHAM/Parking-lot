b17f02e6c2cc87713f55d0268b76452030654702
Viswanatha Velayutham S <112478839+VISWANATHAVELAYUTHAM@users.noreply.github.com>
2024-08-06 17:25:51 +0530

    Update ParkingFloor.java

--- src/com/parkinglot/ParkingFloor.java
+++ src/com/parkinglot/ParkingFloor.java
@@ -1,1 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class ParkingFloor {
+    private int floorNumber;
+    private List<ParkingSlot> slots;
+
+    public ParkingFloor(int floorNumber) {
+        this.floorNumber = floorNumber;
+        this.slots = new ArrayList<>();
+    }
+
+    public void addParkingSlot(ParkingSlot slot) {
+        slots.add(slot);
+    }
+
+    public List<ParkingSlot> getSlots() {
+        return slots;
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+
+    public ParkingSlot findAvailableSlot(ParkingSlotType type) {
+        for (ParkingSlot slot : slots) {
+            if (!slot.isOccupied() && slot.getType() == type) {
+                return slot;
+            }
+        }
+        return null;
+    }
+}
 
