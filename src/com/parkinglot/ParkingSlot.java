83625326f15e55e453e1431500a10f4e9a12c6b1
Viswanatha Velayutham S <112478839+VISWANATHAVELAYUTHAM@users.noreply.github.com>
2024-08-06 17:26:14 +0530

    Update ParkingSlot.java

--- src/com/parkinglot/ParkingSlot.java
+++ src/com/parkinglot/ParkingSlot.java
@@ -1,1 +1,32 @@
+class ParkingSlot {
+    private int slotNumber;
+    private ParkingSlotType type;
+    private boolean isOccupied;
+
+    public ParkingSlot(int slotNumber, ParkingSlotType type) {
+        this.slotNumber = slotNumber;
+        this.type = type;
+        this.isOccupied = false;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void park() {
+        this.isOccupied = true;
+    }
+
+    public void vacate() {
+        this.isOccupied = false;
+    }
+
+    public ParkingSlotType getType() {
+        return type;
+    }
+
+    public int getSlotNumber() {
+        return slotNumber;
+    }
+}
 
