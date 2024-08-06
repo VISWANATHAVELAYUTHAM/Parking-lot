462efdbb29b725c264fe4c8c9370663e32074b70
Viswanatha Velayutham S <112478839+VISWANATHAVELAYUTHAM@users.noreply.github.com>
2024-08-06 17:25:34 +0530

    Update Ticket.java

--- src/com/parkinglot/Ticket.java
+++ src/com/parkinglot/Ticket.java
@@ -1,1 +1,26 @@
+import java.time.LocalDateTime;
+
+class Ticket {
+    private Vehicle vehicle;
+    private LocalDateTime entryTime;
+    private ParkingSlot slot;
+
+    public Ticket(Vehicle vehicle, ParkingSlot slot) {
+        this.vehicle = vehicle;
+        this.slot = slot;
+        this.entryTime = LocalDateTime.now();
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+
+    public ParkingSlot getSlot() {
+        return slot;
+    }
+}
 
