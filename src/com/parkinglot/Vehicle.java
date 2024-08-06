19a808789734d543282f44da10536f32894f2cf9
Viswanatha Velayutham S <112478839+VISWANATHAVELAYUTHAM@users.noreply.github.com>
2024-08-06 17:26:38 +0530

    Update Vehicle.java

--- src/com/parkinglot/Vehicle.java
+++ src/com/parkinglot/Vehicle.java
@@ -1,1 +1,18 @@
+class Vehicle {
+    private String licensePlate;
+    private ParkingSlotType type;
+
+    public Vehicle(String licensePlate, ParkingSlotType type) {
+        this.licensePlate = licensePlate;
+        this.type = type;
+    }
+
+    public ParkingSlotType getType() {
+        return type;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+}
 
